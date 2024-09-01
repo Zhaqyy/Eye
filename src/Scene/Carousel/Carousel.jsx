46d82e3dae845deab9dc20e68abee028b6852894
zhaqyy <zaqrashyy7@gmail.com>
2024-09-01 08:07:59 +0100

    extra shader customization for plane (more glitchy and convex-y)

--- src/Scene/Carousel/Carousel.jsx
+++ src/Scene/Carousel/Carousel.jsx
@@ -11,8 +11,8 @@ import images from './data/images'
 Plane Settings
 ------------------------------*/
 const planeSettings = {
-  width: 6.5,
-  height: 3.5,
+  width: 5.5,
+  height: 3,
   gap: 10
 }
 
@@ -33,10 +33,10 @@ export const lerp = (v0, v1, t) => v0 * (1 - t) + v1 * t
 Get Piramidal Index
 --------------------*/
 // Returns an array of decreasing index values in a pyramid shape, starting from the specified index with the highest value. These indices are often used to create overlapping effects among elements.
-export const getPiramidalIndex = (array, index) =>
-  array.map((_, i) =>
-    index === i ? array.length : array.length - Math.abs(index - i)
-  )
+// export const getPiramidalIndex = (array, index) =>
+//   array.map((_, i) =>
+//     index === i ? array.length : array.length - Math.abs(index - i)
+//   )
 
 
 /*------------------------------
@@ -56,7 +56,7 @@ const Carousel = () => {
   const progress = useRef(0)
   const startX = useRef(0)
   const isDown = useRef(false)
-  const speedWheel = 0.02
+  const speedWheel = 0.01
   const speedDrag = -0.01
   const oldProgress = useRef(0)
   const speed = useRef(0)
@@ -68,10 +68,10 @@ const Carousel = () => {
   Diaplay Items
   --------------------*/
   const displayItems = (item, index, active) => {
-    const piramidalIndex = getPiramidalIndex($items, active)[index]
+    // const piramidalIndex = getPiramidalIndex($items, active)[index]
     gsap.to(item.position, {
       x: (index - active) * (planeSettings.width + planeSettings.gap),
-      y: $items.length * -0.1 + piramidalIndex * 0.1
+      // y: $items.length * -0.1  * 0.1
     })
   }
 
