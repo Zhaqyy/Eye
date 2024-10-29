64c77dc577116709e81555610fe205efbaad26e4
zhaqyy <zaqrashyy7@gmail.com>
2024-10-30 00:39:21 +0100

    added better fake glow

--- src/Scene/BgScene/Pillars.jsx
+++ src/Scene/BgScene/Pillars.jsx
@@ -1,8 +1,10 @@
 import * as THREE from "three";
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, forwardRef, memo } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import { EllipseCurve, CatmullRomCurve3, Vector3 } from "three";
 import FakeGlowMaterial from "../Helper/FakeGlowMaterial.jsx";
+import { Billboard, Shadow, Sparkles } from "@react-three/drei";
+import { Depth, LayerMaterial } from "lamina";
 function Pillar(props) {
   const sphereRef = useRef();
   const { pointer } = useThree();
@@ -46,20 +48,8 @@ function Pillar(props) {
         <circleGeometry args={[0.25, 5]} />
         <meshPhongMaterial color={"#242424"} shininess={100} />
       </mesh>
-
       {/* Sphere following the path */}
-      <mesh ref={sphereRef} position={[0, 0, 0]}>
-        <sphereGeometry args={[0.25, 16, 16]} />
-        {/* <meshStandardMaterial color={"#ff6347"} emissive={"#ff6347"} emissiveIntensity={0.6} /> */}
-        <FakeGlowMaterial
-          falloff={0.25}
-          glowInternalRadius={1.0}
-          glowColor={"#ff0000"}
-          glowSharpness={1}
-          side={"THREE.FrontSide"}
-          opacity={1.0}
-        />
-      </mesh>
+      <Sphere ref={sphereRef} color='#ff0000' amount={20} emissive='#ff0000' glow='#ff0000' size={0.25} />
     </group>
   );
 }
@@ -73,3 +63,45 @@ export default function Pillars() {
     </>
   );
 }
+
+// <FakeGlowMaterial
+//   falloff={0.25}
+//   glowInternalRadius={1.0}
+//   glowColor={"#ff0000"}
+//   glowSharpness={1}
+//   side={"THREE.FrontSide"}
+//   opacity={1.0}
+// />
+
+// Sphere with memoization for improved performance
+const Sphere = React.memo(
+  forwardRef(({ size = 1, amount = 50, color = "white", emissive, glow, ...props }, ref) => (
+    <mesh {...props} ref={ref}>
+      <sphereGeometry args={[size, 64, 64]} />
+      <meshStandardMaterial roughness={0} color={color} emissive={emissive || color} envMapIntensity={0.2} />
+      <Glow scale={0.75} near={-25} color={glow || emissive || color} />
+    </mesh>
+  ))
+);
+
+// Glow with memoization
+const Glow = memo(({ color, scale = 0.5, near = -2, far = 1.4 }) => (
+  <Billboard>
+    <mesh>
+      <circleGeometry args={[2 * scale, 16]} />
+      <LayerMaterial
+        transparent
+        depthWrite={false}
+        blending={THREE.CustomBlending}
+        blendEquation={THREE.AddEquation}
+        blendSrc={THREE.SrcAlphaFactor}
+        blendDst={THREE.DstAlphaFactor}
+      >
+        <Depth colorA={color} colorB='black' alpha={1} mode='normal' near={near * scale} far={far * scale} origin={[0, 0, 0]} />
+        <Depth colorA={color} colorB='black' alpha={0.5} mode='add' near={-40 * scale} far={far * 1.2 * scale} origin={[0, 0, 0]} />
+        <Depth colorA={color} colorB='black' alpha={1} mode='add' near={-15 * scale} far={far * 0.7 * scale} origin={[0, 0, 0]} />
+        <Depth colorA={color} colorB='black' alpha={1} mode='add' near={-10 * scale} far={far * 0.68 * scale} origin={[0, 0, 0]} />
+      </LayerMaterial>
+    </mesh>
+  </Billboard>
+));
